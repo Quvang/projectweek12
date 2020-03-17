85d81baad6ede7e5b113b83077c15c1815c0b363
Sune Quvang Iversen <suneq18@gmail.com>
2020-03-17 10:55:35 +0100

    edit index.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Leg Med Express' });
+  res.render('index', { title: 'World MAP' });
 });
 
 /* GET Country page. */
 router.get('/country', function(req, res, next) {
-    worldHandler.retrieveAllCountries(req, res, next);  // get data from
+      //worldHandler.retrieveAllCountries(req, res, next);  // get data from
+      res.render('index', { title: '' });
 });
 
 /* POST Country PAGE */
 
-router.post('/country', function(req, res, next) {
+/*router.post('/country', function(req, res, next) {
       worldHandler.retrieveChosenCountry(req, res, next);
-});
+      res.render('index', { title: 'Leg Med Express' });
+});*/
 
 /* GET City page. */
 router.get('/city', function(req, res, next) {
