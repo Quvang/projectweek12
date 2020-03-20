db4dacc8b60cefba7a44f36a507972f14db64181
Awienberg <a.wienberg@hotmail.com>
2020-03-20 11:40:48 +0100

    reroll!

--- public/js/page.js
+++ public/js/page.js
@@ -2,54 +2,107 @@
 import {$} from "./modules/nQuery.js";
 import {Ajax} from "./modules/Ajax.js";
 
-/*
- * Event handler for button - create ajax object and get data
- */
+const getContinents = function(ev) {
+    let req = Object.create(Ajax);
+    req.init();
+    req.getFile("/continents", showContinents);
+};
+const getCountries = function(ev) {
+    let req = Object.create(Ajax);
+    req.init();
+    req.getFile(`/countries/${ev.target.value}`, showCountries);
+};
 const getCities = function(ev) {
     let req = Object.create(Ajax);
     req.init();
-    req.getFile(`/country/:cont/:countrycode`, showCities);
+    req.getFile(`/cities/${ev.target.value}`, showCities);
 };
-
 /*
  * callback function for the above AJaX
  */
-const showCities = function (e) {
+const showContinents = function(e) {
     /*
      * here you put the ajax response onto your page DOM
      */
     console.log(e.target.getResponseHeader("Content-Type"));
-    let element = $("getcities");
+    let element = $("contdata");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    let div = document.createElement("div");
+    let continents = JSON.parse(e.target.responseText);
+    let sel = document.createElement('select');
+    sel.setAttribute('id', 'chooseContinent');
+    sel.addEventListener('change', getCountries);
+    continents.forEach(function(continent) {
+        let opt = document.createElement('option');
+        let opttext = document.createTextNode(continent.name);
+        opt.appendChild(opttext);
+        sel.appendChild(opt);
+    });
+    div.appendChild(sel);
+    $("contdata").appendChild(div);
+}
+
+const showCountries = function (e) {
+    /*
+     * here you put the ajax response onto your page DOM
+     */
+    console.log(e.target.getResponseHeader("Content-Type"));
+    let element = $("countdata");
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
     let div = document.createElement("div");
     let h3 = document.createElement('h3');
-    let txt = document.createTextNode('Choose City');
+    let txt = document.createTextNode('Countries');
+    h3.appendChild(txt);
+    div.appendChild(h3);
+    let countries = JSON.parse(e.target.responseText);
+    let sel = document.createElement('select');
+    sel.setAttribute('id', 'chooseCountry');
+    sel.addEventListener('change', getCities);
+    countries.forEach(function(country) {
+        let opt = document.createElement('option');
+        let opttext = document.createTextNode(country.name);
+        opt.addEventListener('click', getCities);
+        opt.setAttribute('value', country.code);
+        opt.appendChild(opttext);
+        sel.appendChild(opt);
+    });
+    div.appendChild(sel);
+    $("countdata").appendChild(div);
+};
+
+const showCities = function (e) {
+    /*
+     * here you put the ajax response onto your page DOM
+     */
+    console.log(e.target.getResponseHeader("Content-Type"));
+    let element = $("citydata");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    let div = document.createElement("div");
+    let h3 = document.createElement('h3');
+    let txt = document.createTextNode('Cities');
     h3.appendChild(txt);
     div.appendChild(h3);
     let cities = JSON.parse(e.target.responseText);
     let sel = document.createElement('select');
     sel.setAttribute('id', 'chooseCity');
-    sel.addEventListener('change', getCities);
-    var btn = document.createElement("BUTTON");   // Create a <button> element
-    btn.innerHTML = "Click me";
-    btn.setAttribute('id', 'gdat')
     cities.forEach(function(city) {
         let opt = document.createElement('option');
-        opt.setAttribute('value', country.code);
         let opttext = document.createTextNode(city.name);
         opt.appendChild(opttext);
         sel.appendChild(opt);
     });
     div.appendChild(sel);
-    div.appendChild(btn);
-
-    $("getcities").appendChild(div);
+    $("citydata").appendChild(div);
 };
 
 const showStarter = function () {
-    $('gcit').addEventListener('click', getCities);
+    $('gcont').addEventListener('click', getContinents);
 }
 
 window.addEventListener("load", showStarter);                   // kick off JS
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,16 @@
 var express = require('express');
 var router = express.Router();
 /* Scheamas */
-const modcontinents = require('../models/handleContinents');
-const modCities = require('../models/handleCities');
-const modCountry = require('../models/handleCountries');
+const modContinent = require("../models/handleContinents");
+const modCountry = require("../models/handleCountries");
+const modCities = require("../models/handleCities");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Map of the World' });
 });
 
-/* Click on continent. */
-router.get('/country/:cont', async function(req, res, next) {
-    let country = await modCountry.getCountries({continent: req.params.cont}, { sort: { name: 1 } });
-    res.render('country', { title: 'Country Data', country: country });
-});
-
-/* Chose Country - Show cities. */
-router.get('/country/:cont/:countrycode', async function(req, res, next) {
-    let cities = await modCities.getCities({countrycode: req.params.countrycode}, { sort: { name: 1 } });
-    res.json(cities);
-});
 
 /* GET Country page. */
 router.get('/country', async function(req, res, next) {
@@ -28,6 +18,32 @@ router.get('/country', async function(req, res, next) {
     res.render('country', { title: 'Country Data', country: country });
 });
 
+router.get('/continents', async function(req, res, next) {
+    let continents = await modContinent.getContinents({}, {sort: {name: 1}});
+    res.json(continents);
+});
+
+router.get('/countries/:cont', async function(req, res, next) {
+    let countries = await modCountry.getCountries({continent: req.params.cont}, {sort: {name: 1}});
+    res.json(countries);
+});
+
+router.get('/cities/:city', async function(req, res, next) {
+    let cities = await modCities.getCities({countrycode: req.params.city}, {sort: {name: 1}});
+    res.json(cities);
+});
+
+/* Click on continent. */
+router.get('/country/:cont', async function(req, res, next) {
+    let country = await modCountry.getCountries({continent: req.params.cont}, { sort: { name: 1 } });
+    res.render('country', { title: 'Country Data', country: country });
+});
+/*
+router.get('/cities/', async function(req, res, next) {
+    let cities = await modCities.getCities({country: req.params.code}, {sort: {name: 1}});
+    res.json(cities);
+});
+*/
 /* GET City page. */
 router.get('/city', function(req, res, next) {
     res.render('city', { title: 'City Data' });
