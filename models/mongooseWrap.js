930200f1eb57a11a9608d12928fc0c872e2b89f9
Sune Quvang Iversen <suneq18@gmail.com>
2020-03-20 14:38:47 +0100

    ny wrapper

--- models/mongooseWrap.js
+++ models/mongooseWrap.js
@@ -4,6 +4,8 @@
  */
 const mongoose = require('mongoose');
 
+
+
 // mongoose.set('debug', true);
 // mongoose.set('debug', { color: false });
 
@@ -33,8 +35,34 @@ exports.upsert = async function(url, dbn, obj, query, chk) {
     await mongoose.connect(constr, conparam);
     const db = mongoose.connection;
     let stuff = null;
+    let newquery = query.toObject();            // ny
+    delete newquery._id;                        // ny
     try {
-        stuff = await obj.findOneAndUpdate(chk, {"$set": query}, {upsert: true});
+      stuff = await obj.updateOne(chk, newquery, {        // ændret
+      upsert: true                                        // 1 linje fjernet, må sikkert gerne være der
+      });
+    //try {
+    //    stuff = await obj.updateOne(chk, query, {
+    //        upsert: true,
+    //        new: true
+    //    });
+    } catch(err) {
+        console.log(error);
+    } finally {
+        db.close();
+        return stuff;
+    }
+}
+
+//Sletter efter name - testet :-)
+exports.remove = async function(url, dbn, obj, name) {
+    const constr = `mongodb://${url}:27017/${dbn}`;
+    await mongoose.connect(constr, conparam);
+    const db = mongoose.connection;
+    let stuff = null;
+    try {
+        stuff = await obj.deleteOne(name, (err) => {});
+        console.log("Successful deletion");
     } catch(err) {
         console.log(error);
     } finally {
